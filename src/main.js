625ac1d94113702c617aef8f501839ac3d43aa77
Nick Moore <nick65@gmail.com>
2015-07-22 20:15:34 -0500

    Initial

---
+++ src/main.js
@@ -0,0 +1,11 @@
+
+
+function main() {
+  let content = document.createElement("div");
+
+  document.body.appendChild(content);
+
+  content.innerHTML = "<div class='jumbotron'><h1>Hello!</h1></div>";
+}
+
+main();
